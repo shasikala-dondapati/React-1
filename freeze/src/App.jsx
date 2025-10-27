9e4f97bbf0c7eee673ae62152d05663d52ce3e8c
shasikala-dondapati <shashidondapati@gmail.com>
2025-10-27 18:50:01 +0530

    React from Day5

---
+++ freeze/src/App.jsx
@@ -0,0 +1,19 @@
+import Header from "./Header";
+import Banner from "./Banner";
+import Body from "./Body";
+import Body1 from "./Body1";
+import Footer from "./Footer";
+import './App.css'
+
+function App(){
+  return(
+    <div>
+        <Header />
+        <Banner />
+        <Body />
+        <Body1 />
+        <Footer />
+    </div>
+  )
+}
+export default App;
---
+++ freeze/src/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer(){
+    return(
+        <div id="ftr">
+           <div id="imgs" >
+            <img src="https://img.icons8.com/?size=100&id=118468&format=png&color=000000" />
+            <img src="https://img.icons8.com/?size=100&id=437&format=png&color=000000" />
+            <img src="https://img.icons8.com/?size=100&id=8867&format=png&color=000000" />
+            <img src="https://img.icons8.com/?size=100&id=1481&format=png&color=000000" />
+            </div>
+            <p style={{textAlign:"center"}}>© 2023 Freeeze. All Rights Reserved.</p>
+        </div>
+    )
+}
+export default Footer;
